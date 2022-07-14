6be52fb95bbefea916cc2ea09df4e15c5d502f9e
Valentyna768 <85171952+Valentyna768@users.noreply.github.com>
2022-07-14 16:09:29 +0300

    Update Sitemap.js

--- Sitemap.js
+++ Sitemap.js
@@ -67,4 +67,110 @@ describe('Crossdomain', () => {
                 await expect(message).toHaveTextContaining('sitemap is generated')
             });
         });
+        //  //Test 4 Sitemap status code 200// - не розумію як встановити/зробити щоб запит проходив
+        //  describe('Sitemap status code 200', () => {
+        //     it('Sitemap status code 200', async () => {    
+        //         await expect(mock).toBeRequestedWith({
+        //         url: 'https://casino-kit-prod.site/sitemap_main.xml',                                 
+        //         statusCode: 200,              //чи пройде?
+        //     });
+        // });
+        //Test 5 Links are present in Sitemap//
+    describe('Links are present in Sitemap', () => {
+        it('check if post, page, casino, slot links are present', async () => {
+        browser.url('https://qa.casino-kit-prod.site/sitemap_main.xml');
+        const casinoUrl =$('body > urlset > url:nth-child(2) > loc');
+        await expect(casinoUrl).toBePresent(); 
+        await expect(casinoUrl).toHaveTextContaining('https://qa.casino-kit-prod.site/zigzag777-casino/')
+        const pageUrl =$('body > urlset > url:nth-child(42) > loc');
+        await expect(pageUrl).toBePresent();
+        await expect(pageUrl).toHaveTextContaining('https://qa.casino-kit-prod.site/how-to-play-roulette/')
+        const postUrl =$('body > urlset > url:nth-child(74) > loc');
+        await expect(postUrl).toBePresent();
+        await expect(postUrl).toHaveTextContaining('https://qa.casino-kit-prod.site/postslug5/post-for-test/')
+        const slotUrl =$('body > urlset > url:nth-child(97) > loc');
+        await expect(slotUrl).toBePresent();
+        await expect(slotUrl).toHaveTextContaining('https://qa.casino-kit-prod.site/testslot5/pontoon-gamesos/')
+        });
+    });
+    //Test 6 Open admin crossdomain settings again//
+    describe('Open admin crossdomain settings', () => {
+        it('should open crossdomain setting page in admin', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/options-general.php?page=sitemap-custom-polylang');
+        });
+    }); 
+     //Test 7 Checkbox in Category Payment Software//
+     describe('Check selcted Checkbox in Category Payment Software', () => {
+        it('should check Checkbox in Category Payment Software', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/options-general.php?page=sitemap-custom-polylang');
+            //await browser.pause(3000)-спробувать без паузи
+            const selectedTaxonomies=$('#wpbody-content > div:nth-child(6) > div:nth-child(1) > form > div:nth-child(2) > input[type=hidden]')
+            let selectedTaxonomiesChecked = await selectedTaxonomies.getAttribute('value');
+            await assert.strictEqual(selectedTaxonomiesChecked, 'category,payments,software' )
+            const saveSitemapBtn =$('#wpbody-content > div:nth-child(6) > div:nth-child(1) > form > button')
+            saveSitemapBtn.click();
+            await browser.pause(3000);
+            });
+        });
+        //Test 8 Open category page in admin//
+     describe('Open category page in admin', () => {
+        it('should Open category page in admin', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=category&tag_ID=1814&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dcategory%26post_type%3Dcasino');
+        });
+    });
+    //Test 9 Check selected checkbox and update category page//
+    describe('Check selected checkbox and update category page', () => {
+        it('should check selected checkbox and update category page', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=category&tag_ID=1814&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dcategory%26post_type%3Dcasino');
+            const checkbox=$('#edittag > div.form-field.term-group > input[type=checkbox]')
+            await expect(checkbox).toHaveAttr('checked')
+            const updateBtn=$('#edittag > div.edit-tag-actions > input')
+            updateBtn.click();
+            await browser.pause(3000);
+        });
+    });
+    //Test 10 Open payment page in admin//
+    describe('Open payment page in admin', () => {
+        it('should Open payment page in admin', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=payments&tag_ID=78&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dpayments%26post_type%3Dcasino');
+        });
+    });
+     //Test 11 Check selected checkbox and update payment page//
+     describe('Check selected checkbox and update payment page', () => {
+        it('Check selected checkbox and update payment page', async () => {
+            browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=payments&tag_ID=78&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dpayments%26post_type%3Dcasino');
+            const checkbox=$('#edittag > div.form-field.term-group > input[type=checkbox]')
+            await expect(checkbox).toHaveAttr('checked')
+            const updateBtn=$('#edittag > div.edit-tag-actions > input')
+            updateBtn.click();
+            await browser.pause(3000);
+        });
+    });
+//Test 12 Open sowtware page in admin//
+describe('Open sowtware page in admin', () => {
+    it('should Open sowtware page in admin', async () => {
+        browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=software&tag_ID=1863&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dsoftware%26post_type%3Dcasino');
+    });
+});
+ //Test 12 Check selected checkbox and update software page//
+ describe('Check selected checkbox and update software page', () => {
+    it('Check selected checkbox and update software page', async () => {
+        browser.url('https://qa.casino-kit-prod.site/wp-admin/term.php?taxonomy=software&tag_ID=1863&post_type=casino&wp_http_referer=%2Fwp-admin%2Fedit-tags.php%3Ftaxonomy%3Dsoftware%26post_type%3Dcasino');
+        const checkbox=$('#edittag > div.form-field.term-group > input[type=checkbox]')
+        await expect(checkbox).toHaveAttr('checked')
+        const updateBtn=$('#edittag > div.edit-tag-actions > input')
+        updateBtn.click();
+        await browser.pause(3000);
+    });
+});
+//Test 13 Open settings page in admin//
+describe('Open settings page in admin', () => {
+    it('should Open settings page in admin', async () => {
+        browser.url('https://qa.casino-kit-prod.site/wp-admin/options-general.php?page=sitemap-custom-polylang');
+    });
+});
+ 
+
+
+    
     
